cb3d648a1d250e1be15ce2e5176e990cb6aa0c66
Rehmanvirk <rehmanvirk318@gmail.com>
2025-05-10 21:18:57 +0500

    First Message

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "chitchat-88e51.firebaseapp.com",
+  projectId: "chitchat-88e51",
+  storageBucket: "chitchat-88e51.firebasestorage.app",
+  messagingSenderId: "684350726602",
+  appId: "1:684350726602:web:27c1b27c2c840592096f33",
+  measurementId: "G-J2V1WXBEWH"
+};
+
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
